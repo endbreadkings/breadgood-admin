87888b919e3bf8bcd6b360e3bb84a407cd5cce9d
김도영 <doyoung0205@naver.com>
2022-04-11 20:52:58 +0900

    remove travis && add github actions

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ const config = {
   devServer: {
     overlay: false
   },
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/endbreadkings.github.io/breadgood-admin/"
-      : "",
-  outputDir: "docs",
+  // publicPath:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/endbreadkings.github.io/breadgood-admin/"
+  //     : "",
+  // outputDir: "docs",
   transpileDependencies: ["vuetify"]
 };
 //productionSourceMap: false
