ac20abd7287ad7902f49cc5ce9919c39413ec9fa
김도영 <doyoung0205@naver.com>
2022-04-11 21:16:11 +0900

    Updates

--- src/api/auth.js
+++
@@ -1,9 +0,0 @@
-// 로그인, 회원 가입, (ex) 회원 탈퇴
-import { instance } from "./index";
-
-// 로그인 API
-function loginUser(userData) {
-  return instance.post("user/signin", userData);
-}
-
-export { loginUser };
