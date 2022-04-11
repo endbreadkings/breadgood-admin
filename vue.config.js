77ee47f8023abafc43dbf900629d2f7635fff469
김도영 <doyoung0205@naver.com>
2022-04-11 21:20:12 +0900

    fix : deploy config

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ const config = {
   devServer: {
     overlay: false
   },
-  // publicPath:
-  //   process.env.NODE_ENV === "production"
-  //     ? "/endbreadkings.github.io/breadgood-admin/"
-  //     : "",
-  // outputDir: "docs",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/endbreadkings.github.io/breadgood-admin/"
+      : "",
+  outputDir: "dist",
   transpileDependencies: ["vuetify"]
 };
 //productionSourceMap: false
